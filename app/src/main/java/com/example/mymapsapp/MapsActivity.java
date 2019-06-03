83c66ba24aaaaec48abf2c04a693762afb0b42ad
Derek-Wen <derek.hz.wen@gmail.com>
2019-06-03 13:07:43 -0700

    Update MapsActivity.java

--- app/src/main/java/com/example/mymapsapp/MapsActivity.java
+++ app/src/main/java/com/example/mymapsapp/MapsActivity.java
@@ -39,8 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng SanDiego = new LatLng(32.70, 242.82);
+        mMap.addMarker(new MarkerOptions().position(SanDiego).title("Marker in San Diego"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SanDiego));
     }
 }
+//setMyLocationEnabled
